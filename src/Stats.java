cf4f30f57ff5089043bc4e2ab3ee4a90b5f74b29
kevin7254 <51020163+kevin7254@users.noreply.github.com>
2019-05-29 00:28:37 +0200

    Fixed statistics for all information.

--- src/Stats.java
+++ src/Stats.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Stats {
@@ -43,22 +45,30 @@ public class Stats {
         this.playerWins += 1;
         this.totalPlayerWins += 1;
         this.totalGamesPlayed += 1;
+        this.totalMoneyBetted += i;
+        this.totalMoneyWon += i;
     }
 
     public void gameLost(double i) {
         this.playerMoney -= i;
         this.gamesPlayed += 1;
         this.totalGamesPlayed += 1;
+        this.totalMoneyBetted += i;
     }
 
     public double winPercentage() {
         return (this.playerWins / this.gamesPlayed);
     }
 
-    public void writeToFile() {
+    public void writeToFile() { //TODO: fixa all-time stats för resten oxå
         try {
-            FileWriter writer = new FileWriter("stats.txt");
-            writer.write("Total games played: "+ this.totalGamesPlayed);
+            FileWriter writer = new FileWriter("stats/totalGames.txt");
+
+            writer.write("Total games played: " + this.totalGamesPlayed);
+            writer.write("\nTotal games won: " + this.totalPlayerWins);
+            writer.write("\nTotal money betted: " + this.totalMoneyBetted);
+            writer.write("\nTotal money won: " + this.totalMoneyWon);
+
             writer.close();
             System.out.println("Success writing");
         } catch (Exception e) {
@@ -69,12 +79,21 @@ public class Stats {
 
     public void readFile() {
         try {
-            FileReader fin = new FileReader("stats.txt");
+            FileReader fin = new FileReader("stats/totalGames.txt");
             Scanner scanner = new Scanner(fin);
-            String str = scanner.nextLine();
-            this.totalGamesPlayed += Double.parseDouble(str.replace("Total games played: ", ""));
+            List<String> list = new ArrayList<String>();
+
+            while (scanner.hasNextLine()) {
+                list.add(scanner.nextLine());
+            }
+            scanner.close();
+            this.totalGamesPlayed += Double.parseDouble(list.get(0).replace("Total games played: ", ""));
+            this.totalPlayerWins += Double.parseDouble(list.get(1).replace("Total games won: ", ""));
+            this.totalMoneyBetted += Double.parseDouble(list.get(2).replace("Total money betted: ", ""));
+            this.totalMoneyWon += Double.parseDouble(list.get(3).replace("Total money won: ", ""));
         } catch (Exception e) {
             e.getStackTrace();
         }
+
     }
 }
